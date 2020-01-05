99f15568dfa88cfac4269c497d76a161bf9f5e11
KenkenGoda <shohei.goda.12.1@gmail.com>
2020-01-05 23:47:54 +0900

    completed methods-and-interfaces section

---
+++ methods-and-interfaces/exercise-errors.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z * z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	fmt.Sprint(float64(e))
+	return fmt.Sprintf("cannot Sqrt negative number: %g", e)
+}
